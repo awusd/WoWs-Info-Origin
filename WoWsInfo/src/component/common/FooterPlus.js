6cbbc20515e13b80d9bdf753075ba7dbce5858d6
Yiheng <HenryQuan@users.noreply.github.com>
2019-03-17 03:41:57 +1100

    make FooterPlus flay

--- WoWsInfo/src/component/common/FooterPlus.js
+++ WoWsInfo/src/component/common/FooterPlus.js
@@ -5,26 +5,18 @@
  */
 
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import { ThemeBackColour } from '../../value/colour';
 
 class FooterPlus extends Component {
   render() {
-    const { similarView } = styles;
     const { children, style } = this.props;
     return (
-      <View style={[similarView, ThemeBackColour(), style]}>
+      <View style={[ThemeBackColour(), style]}>
         {children}
       </View>
     )
   };
 }
-
-const styles = StyleSheet.create({
-  similarView: {
-    borderTopLeftRadius: 16,
-    borderTopRightRadius: 16,
-  },
-});
 
 export { FooterPlus };
